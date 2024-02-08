7a398fd8ce82b5a2376edc796496ae7c3780f884
sopicard <sogautrelet@gmail.com>
2024-02-08 15:46:11 +0100

    update all features of staff-list with material-ui

--- src/pages/StaffList.jsx
+++ src/pages/StaffList.jsx
@@ -1,43 +1,152 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAppContext } from '../contexts/AppContext'
 import { useNavigate } from 'react-router-dom'
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Button, Typography, TextField, IconButton, Box, MenuItem } from '@mui/material'
+import { ArrowDropDown, ArrowDropUp } from '@mui/icons-material'
 import FieldConfig from '../components/FieldConfig'
 
 const formatDate = (date) => {
-  const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
-  return new Date(date).toLocaleDateString('fr-FR', options);
+  const options = { day: '2-digit', month: '2-digit', year: 'numeric' }
+  return new Date(date).toLocaleDateString('fr-FR', options)
 }
 
 const StaffList = () => {
   const { employeesData } = useAppContext()
   const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('')
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+  const [orderBy, setOrderBy] = useState('')
+  const [order, setOrder] = useState('asc')
 
   useEffect(() => {}, [employeesData])
+
+  const handleRequestSort = (property) => {
+    const isAsc = orderBy === property && order === 'asc'
+    setOrder(isAsc ? 'desc' : 'asc')
+    setOrderBy(property)
+  }
+
+  const sortedData = orderBy
+    ? employeesData.slice().sort((a, b) => {
+        if (order === 'asc') {
+          return a[orderBy] < b[orderBy] ? -1 : 1
+        } else {
+          return b[orderBy] < a[orderBy] ? -1 : 1
+        }
+      })
+    : employeesData
+
+  const filteredData = sortedData.filter(employee => {
+    return Object.values(employee).some(value =>
+      value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  })
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
 
   return (
     <div>
       <main>
-      <h2>Current Employees</h2>
-      <table>
-        <thead>
-          <tr>
-            {FieldConfig.map((field) => (
-              <th key={field.name}>{field.label}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-        {employeesData.map((employee, index) => (
-          <tr key={index}>
-            {FieldConfig.map((field) => (
-              <td key={field.name}>{field.type === 'date' ? formatDate(employee[field.name]) : employee[field.name]}</td>
-            ))}
-          </tr>
-        ))}
-
-        </tbody>
-      </table>
-      <button onClick={() => navigate('/')}>Home</button>
+        <Typography variant="h4" gutterBottom>Current Employees</Typography>
+        <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
+          <TextField
+            className= "staff-list__search"
+            label="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            variant="outlined"
+            size="small"
+          />
+          <Box display="flex" flexDirection="rows" alignItems="center">
+            <Typography variant="body2" component="span" mr={1}>
+              Show
+            </Typography>
+            <TextField
+              className="staff-list__select"
+              select
+              value={rowsPerPage}
+              onChange={handleChangeRowsPerPage}
+              variant="outlined"
+              size="small"
+            >
+              {[10, 25, 50, 100].map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Typography variant="body2" component="span" ml={1}>
+              entries
+            </Typography>
+          </Box>
+        </Box>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {FieldConfig.map((field) => (
+                  <TableCell key={field.name}>
+                    <Box display="flex" alignItems="center">
+                      {field.label}
+                      <Box ml={1} display="flex" flexDirection="column" alignItems="center">
+                        <IconButton disableRipple onClick={() => handleRequestSort(field.name)}>
+                          {orderBy === field.name && order === 'asc' ? <ArrowDropUp sx={{ fontSize: 'medium', marginBottom: '-8px' }} color="primary" /> : <ArrowDropUp sx={{ fontSize: 'medium', marginBottom: '-8px' }} />}
+                        </IconButton>
+                        <IconButton disableRipple onClick={() => handleRequestSort(field.name)}>
+                          {orderBy === field.name && order === 'desc' ? <ArrowDropDown sx={{ fontSize: 'medium', marginTop: '-8px' }} color="primary" /> : <ArrowDropDown sx={{ fontSize: 'medium', marginTop: '-8px' }} />}
+                        </IconButton>
+                      </Box>
+                    </Box>
+                  </TableCell>               
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((employee, index) => (
+                <TableRow key={index}>
+                  {FieldConfig.map((field) => (
+                    <TableCell key={field.name}>{field.type === 'date' ? formatDate(employee[field.name]) : employee[field.name]}</TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box display="flex" justifyContent="space-between" alignItems="center" pt={1} pl={2}>
+          <Typography variant="body2" component="span">
+            Showing {Math.min(page * rowsPerPage + 1, filteredData.length)} to {Math.min((page + 1) * rowsPerPage, filteredData.length)} of {filteredData.length} entries
+          </Typography>
+          <Box display="flex" alignItems="center">
+            <Button 
+              disabled={page === 0} 
+              onClick={() => handleChangePage(null, page - 1)} 
+              variant="outlined" color="primary" 
+              className={`staff-list__pagination ${page === 0 ? 'current-page' : ''}`}
+              >
+                Previous
+              </Button>
+            <Box component="span" ml={2} mr={2}>
+              Page {page + 1}
+            </Box>
+            <Button 
+              disabled={page >= Math.ceil(filteredData.length / rowsPerPage) - 1} 
+              onClick={() => handleChangePage(null, page + 1)} 
+              variant="outlined" color="primary" 
+              className={`staff-list__pagination ${page === Math.ceil(filteredData.length / rowsPerPage) - 1 ? 'current-page' : ''}`}
+              >
+                Next
+            </Button>
+          </Box>
+        </Box>
+        <Button className="staff-list__button" onClick={() => navigate('/')} variant="outlined" color="primary">Home</Button>
       </main>
     </div>
   )
